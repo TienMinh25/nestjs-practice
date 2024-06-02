15213035e57dad0a0099ecf18643a464a79270ce
TienMinh25 <letienminh2512@gmail.com>
2024-06-02 15:20:01 +0700

    test: write few unit test for authentication and user service

---
+++ src/authentication/tests/authentication.service.spec.ts
@@ -0,0 +1,45 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import User from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { mockedConfigService, mockedJwtService } from 'src/utils/mocks';
+import { AuthenticationService } from '../authentication.service';
+
+describe('The AuthenticationService', () => {
+  let authenticationService: AuthenticationService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        AuthenticationService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+      ],
+    }).compile();
+
+    authenticationService = await module.get<AuthenticationService>(
+      AuthenticationService,
+    );
+  });
+
+  describe('when creating a cookie', () => {
+    it('should return a string', () => {
+      const userId = 1;
+      expect(
+        typeof authenticationService.getCookieWithJwtToken(userId),
+      ).toEqual('string');
+    });
+  });
+});
---
+++ src/utils/mocks/jwt.service.ts
@@ -0,0 +1,5 @@
+const mockedJwtService = {
+  sign: () => '',
+};
+
+export { mockedJwtService };
